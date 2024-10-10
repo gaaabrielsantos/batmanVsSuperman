350c96f00bf15017491020d9f19e14ef1d87b1e1
gaaabrielsantos <grvs.93@gmail.com>
2024-10-10 14:13:11 -0300

    atualizacao

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,26 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
-function styles(){
+// Tarefa para processar estilos
+function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-function images(){
+// Tarefa para otimizar imagens
+function images() {
     return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
+// Tarefa para construir o projeto
+const build = gulp.parallel(styles, images);
 
-exports.default = gulp.parallel(styles, images);
-exports.watch = function(){
+// Exporta as tarefas
+exports.default = build; // Tarefa padrão
+exports.build = build; // Tarefa build
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-}
+};
